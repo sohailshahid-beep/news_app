af3d19c6baaf27c75e854a7c7f75567401692db5
Sohail Shahid <sohail.shahid@codingcops.org>
2025-09-11 14:21:41 +0500

    add commen componenets

--- src/UseNewsContext/news.jsx
+++ src/UseNewsContext/news.jsx
@@ -1,31 +1,27 @@
 import { create } from "zustand";
 
-
 export const useNewsStore = create((set) => ({
   newses: [],
 
+
+  
   addNews: (news) =>
     set((state) => ({
       newses: [...state.newses, news],
     })),
 
-    
+
 
   addBulkNews: (newsArray) =>
-
     set((state) => {
-      const updatedNewses = [...state.newses]; 
+      const updatedNewses = [...state.newses];
 
       newsArray.forEach((newsItem) => {
-
         const index = updatedNewses.findIndex((n) => n.id === newsItem.id);
 
         if (index !== -1) {
-     
           updatedNewses[index] = { ...updatedNewses[index], ...newsItem };
-
         } else {
-       
           updatedNewses.push(newsItem);
         }
       });
@@ -39,15 +35,9 @@ export const useNewsStore = create((set) => ({
       newses: state.newses.filter((u) => u.id !== id),
     })),
 
-  
+
+
+
 }));
-
-
-
-
-
-
-
-
 
 //updateNews: (updatedNews) => // set((state) => ({ // newses: state.newses.map((news) => // news.id === updatedNews.id ? { ...news, ...updatedNews } : news // ), // })),
